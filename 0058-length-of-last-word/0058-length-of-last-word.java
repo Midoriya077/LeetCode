6e0d2cc121ab24adc877a04ade47201da08990e7
Midoriya077 <dishantsharma081@gmail.com>
2026-06-29 13:09:00 +0530

    Time: 0 ms (100%), Space: 43.2 MB (34.02%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] ch =s.trim().split("\\s+");
-        return ch[ch.length-1].length();
+        int count=0;
+        int i=s.length()-1;
+        while(i>=0 && s.charAt(i)==' '){
+            i--;
+        }
+        while(i>=0 && s.charAt(i)!=' '){
+            count++;
+            i--;
+        }
+        return count;
        }
 }
