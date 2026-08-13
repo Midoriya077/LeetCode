dc692bf80cc8b89168b1d0c61cacb04e73a85b3a
Midoriya077 <dishantsharma081@gmail.com>
2026-08-13 17:04:09 +0530

    Time: 1 ms (99.94%), Space: 46.7 MB (82.99%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        int sum=0;
+        for(int num:nums){
+            sum^=num;
         }
-        for(int key: map.keySet()){
-            if(map.get(key)==1){
-                return key;
-            }
-        }
-        return -1;
+        return sum;
     }
 }
